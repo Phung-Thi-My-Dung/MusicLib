84094dd11709426942e119e26284d14d8dc55ee9
NhatLNA <anhnhat909940@gmail.com>
2025-11-21 17:03:14 +0700

    env

---
+++ src/main/java/com/musicLib/Main.java
@@ -0,0 +1,8 @@
+package com.musicLib;  // Phải khớp chính xác
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from MusicLib!");  // Test đơn giản
+        // Code khởi động Swing hoặc app của bạn
+    }
+}
